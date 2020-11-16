fb45c8ce7919761b6be7f32128d6221deea763be
Thomas Claes <r0664186@ucll.be>
2020-11-16 19:26:38 +0100

    roleback errorViewModel

--- CMS/Models/ErrorViewModel.cs
+++ CMS/Models/ErrorViewModel.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CMS.Models
 {
     public class ErrorViewModel
     {
-        private string _requestId;
-        public string RequestId { get { return _requestId; } set { ShowRequestId = true; _requestId = value; } }
-        public bool ShowRequestId;
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
