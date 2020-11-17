a4cc000f107d6b351ab2c8b1a6ad7db10d8fdeac
Thomas Claes <r0664186@ucll.be>
2020-11-17 19:42:45 +0100

    Added nuget packages, created folder data & entities, created course

---
+++ CMS/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CMS.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Data
+{
+    public class DataContext : DbContext
+    {
+        
+        public virtual DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ CMS/Data/Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class Course
+    {
+    }
+}
