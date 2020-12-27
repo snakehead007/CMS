e77f969e5b0dc16f0fd1a479829a77c536e6dfe3
snakehead007 <snakehead007@pm.me>
2020-12-27 16:09:15 +0100

    error on Migration
    Unable to create an object of type 'DataContext'.

---
+++ CMS/Data/Entities/Attachment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class Attachment
+    {
+        public int AttachmentId { get; set; }
+        public string Name { get; set; }
+        public AttachmentVersion CurrentVersion { get; set; }
+        public List<AttachmentVersion> Versions { get; set; }
+    }
+}
--- CMS/Data/Entities/Subject.cs
+++ CMS/Data/Entities/Subject.cs
@@ -1,9 +1,12 @@
-﻿namespace CMS.Data.Entities
+﻿using System.Collections.Generic;
+
+namespace CMS.Data.Entities
 {
     public class Subject
     {
         public int SubjectId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<Attachment> Attachments { get; set; }
     }
 }
