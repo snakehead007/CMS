3e0883b1f12b3d2527d4af85636990a428543b1e
snakehead007 <snakehead007@pm.me>
2021-01-04 21:14:34 +0100

    labo 7 af, niet extra

--- CMS/Controllers/AttachmentController.cs
+++ CMS/Controllers/AttachmentController.cs
@@ -6,17 +6,19 @@ using CMS.Data.Entities;
 using CMS.Interfaces.Repositories;
 using CMS.Mappers;
 using CMS.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMS.Controllers
 {
+    [Authorize]
     public class AttachmentController : Controller
     {
         private readonly IAttachmentRepository _attachmentRepository;
         private readonly FileService _fileService;
         private readonly ICourseRepository _courseRepository;
-        public AttachmentController(ICourseRepository courseRepository,FileService fileService, IAttachmentRepository attachmentRepository)
+        public AttachmentController(ICourseRepository courseRepository, FileService fileService, IAttachmentRepository attachmentRepository)
         {
             _attachmentRepository = attachmentRepository;
             _fileService = fileService;
@@ -31,6 +33,7 @@ namespace CMS.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles ="Admin,Lector")]
         public async Task<IActionResult> UploadAttachment(int subjectId, IFormFile file)
         {
             string virtualPath = await _fileService.SaveFileAsync(file.FileName, file.OpenReadStream());
@@ -41,10 +44,38 @@ namespace CMS.Controllers
                 CreatedOn = DateTime.UtcNow
             };
             var attachment = await _attachmentRepository.createAttachment(file.FileName, version);
-            _courseRepository.AddAttachmentToSubjectAsync(subjectId, attachment);
+            await _courseRepository.AddAttachmentToSubjectAsync(subjectId, attachment);
             return RedirectToAction("ViewAttachment", new { attachmentId = attachment.AttachmentId});
 
         }
 
+        [HttpPost]
+        [Authorize(Roles = "Admin,Lector")]
+        public async Task<IActionResult> UploadVersion(int attachmentId, IFormFile file)
+        {
+            string virtualPath = await _fileService.SaveFileAsync(file.FileName, file.OpenReadStream());
+            var version = new AttachmentVersion
+            {
+                Location = virtualPath,
+                Size = (int)file.Length,
+                CreatedOn = DateTime.UtcNow
+            };
+            var attachment = await _attachmentRepository.AddVersionToAttachmentAsync(attachmentId, version);
+            return RedirectToAction("ViewAttachment", new { attachmentId = attachment.AttachmentId });
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin,Lector")]
+        public async Task<IActionResult> ChangeVersion(int attachmentId, int versionId)
+        {
+            var attachment = await _attachmentRepository.changeVersionOfAttachmentAsync(attachmentId, versionId);
+            return RedirectToAction("ViewAttachment", new { attachmentId = attachment.AttachmentId });
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetFileContent(int attachmentId)
+        {
+            var attachment = await _attachmentRepository.GetAttachmentAsync(attachmentId);
+            var stream = await _fileService.GetFileAsync(attachment.CurrentVersion.Location);
+            return File(stream, "application/octet-stream", attachment.Name);
+        }
     }
 }
